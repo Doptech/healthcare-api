6b326a99e54b40a8c0e7a1d8d5696a511139bfc0
gamingflexer <osurve20ece@student.mes.ac.in>
2023-03-16 19:47:09 +0530

    added config files

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
 import os
 
-MODEL_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'model')
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+MODEL_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'models')
 
 ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
 
