7cbb9f6913c736ca6dfe8f1d3a7a0f2edcf1cd26
gamingflexer <osurve20ece@student.mes.ac.in>
2023-03-17 03:56:20 +0530

    Summarization | Whisper | OCR

--- src/modules/helperFun.py
+++ src/modules/helperFun.py
@@ -1,10 +1,25 @@
 import re
+import whisper
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 class PreProcessor():
     
     def __init__(self, data):
         self.data = data
         
+    def wav_to_transcript(wav_file_path,model_name="base", segments = False):
+        model = whisper.load_model(model_name)
+        result = model.transcribe(wav_file_path)
+        if segments:
+            for segment in result['segments']:
+                segment.pop('tokens')
+                segment.pop('temperature')
+                segment.pop('avg_logprob')
+                segment.pop('compression_ratio')
+                segment.pop('no_speech_prob')
+            return result['segments']
+        return result
+    
     def preprocess(self):
         # clean data using regex
         text = self.data['text']
@@ -18,4 +33,13 @@ class PreProcessor():
         # then we can use retriever
         return self.data
     
-    
+    def summarization(self,ARTICLE_TO_SUMMARIZE):
+        tokenizer = AutoTokenizer.from_pretrained("ccdv/lsg-bart-base-16384-pubmed")
+        model = AutoModelForSeq2SeqLM.from_pretrained("ccdv/lsg-bart-base-16384-pubmed")
+        
+        inputs = tokenizer([ARTICLE_TO_SUMMARIZE], max_length=1024, return_tensors="pt")
+
+        # Generate Summary
+        summary_ids = model.generate(inputs["input_ids"], num_eams=2, min_length=0, max_length=20)
+        tokenizer.batch_decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        return model.generate(inputs["input_ids"], num_eams=2, min_length=0, max_length=20)
---
+++ src/requirements.sh
@@ -0,0 +1,4 @@
+pip install git+https://github.com/openai/whisper.git --no-deps
+python -m spacy download en_core_web_sm
+pip install "layoutparser[ocr]"
+sudo apt install tesseract-ocr
